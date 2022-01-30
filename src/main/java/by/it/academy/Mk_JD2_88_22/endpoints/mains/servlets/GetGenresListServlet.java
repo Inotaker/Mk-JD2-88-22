f84017aefaa6980caf14a7a0e1ac23b955db1511
Inotaker <87884589+Inotaker@users.noreply.github.com>
2022-01-31 01:17:22 +0300

    коммит перед занятием 31.01.2022 все вроде работает) не могу убрать ChoiceWithCount - все ломается не могу иниициализровать переменную poolService через конструкторы - все ломается

---
+++ src/main/java/by/it/academy/Mk_JD2_88_22/endpoints/mains/servlets/GetGenresListServlet.java
@@ -0,0 +1,35 @@
+package by.it.academy.Mk_JD2_88_22.endpoints.mains.servlets;
+
+import by.it.academy.Mk_JD2_88_22.service.PoolService;
+import by.it.academy.Mk_JD2_88_22.service.api.IPoolService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "GetGenresServlet", urlPatterns = "/genres_list")
+public class GetGenresListServlet extends HttpServlet {
+
+    private IPoolService poolService = PoolService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List<String> genres = poolService.getGenres();
+
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+
+        writer.write("<p>Выберите 3 любимых жанра,написав соответсвующие голоса в хэдер \"GENRE_POOL\"</p><br>");
+        for (int i = 0; i < genres.size(); i++) {
+            writer.write("<p>" + (i + 1) + "." + genres.get(i) + "</p>");
+        }
+        writer.write("<p>Напишите допольнительную информацию о себе в хэдер \"GENRE_POOL\"</p><br>");
+    }
+}
